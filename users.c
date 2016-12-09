7e0dfa93d106e94550a260142b930214615cc558
Heit Alexandre <aheit@tc405-009-05.insa-lyon.fr>
2016-12-09 09:31:54 +0100

    5008

--- users.c
+++ users.c
@@ -1,2 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
+
+
+int main(){
+
+
+
+  return(0);
+}
+
+int user{
+
+  signal(SIGINT,arret);
+  signal(SIGHUP,arret);
+  signal(SIGQUIT,arret);
+
+  while(1){
+    int destination=0;
+    int nombrePlaces=0;
+
+    fprintf(stdout,"Entrer la destination de vos rêve SVP:\n");
+    scanf("%d\n",&destination);
+    fprintf(stdout,"Entrer le nombre de places SVP:\n");
+    scanf("%d\n",&nombrePlaces);
+    
+  }
+
+  return(0);
+}
