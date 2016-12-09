e7e09897702592382f23d66364a01b6883ea58a1
Heit Alexandre <aheit@tc405-009-05.insa-lyon.fr>
2016-12-09 11:03:05 +0100

    6440

--- users.c
+++ users.c
@@ -13,9 +13,17 @@ int main(){
 
 int users(){
 
+  int keyMutex2=150;
+  int keyPresence=350;
+  int keyMqueue=400;
+
   signal(SIGINT,stop);
   signal(SIGHUP,stop);
   signal(SIGQUIT,stop);
+
+  open_semaphore(keyMutex2);
+  open_semaphore(keyPresence);
+  open_mqueue(keyMqueue);
 
   while(1){
     int destination=0;
@@ -23,8 +31,7 @@ int users(){
     int myPid=getpid();
     Message *newMessage;
     newMessage=(Message*)malloc(sizeof(Message));
-    open_semaphore(key)
-
+    
     fprintf(stdout,"Entrer la destination de vos rêve SVP:\n");
     scanf("%d\n",&destination);
     fprintf(stdout,"Entrer le nombre de places SVP:\n");
@@ -32,15 +39,21 @@ int users(){
     newMessage->destination=destination;
     newMessage->number=placeNumber;
     newMessage->pid=myPid;
-    
-
+    down(keyMutex2);
+    //deposer le message
+    pause();
+    signal(SIGUSR1,replySignal);
+    signal(SIGUSR2,replySignal);
   }
 
   return(0);
+  
 }
 
- creerMessage(){
-       
-       }
+void replySignal(int sig){
+  if(sig == SIGUSR1)
+    fprintf(stdout,"Réservation confirmée");
 
-
+  if(sig == SIGUSR2)
+    fprintf(stdout,"Probème lors de la réservation");
+}
