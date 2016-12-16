45337d6e723e720ddb05e018f78100319175ea1b
aheit <aheit@localhost.localdomain>
2016-12-16 09:50:49 +0100

    3558

--- tirage.c
+++ tirage.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int tirage(){
 
-	if(int pipe(int descripteur[0])== -1);{
+	signal(SIGINT, stop);
+	signal(SIGHUP,stop);
+	signal(SIGQUIT,stop);
+
+	int pid, descripteur[2];
+	if(pipe(descripteur[2])== -1);{
 		printf("erreur création pipe\n",);
 		exit(-1);
-
 	}
+
+	if((pid = fork()<0){
+		printf("fork failed\n");
+		return -1;
+	}
+	if(pid == 0){ //child
+		close(descripteur[1]);
+	}
+	else{  //parents
+		close(descripteur[0]);
+		printf("Je suis Tirage et je tire un vol aléatoire\n");
+		
+	}
+	close(descripteur[0]);
+
 }
