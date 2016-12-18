9daa00a943469fbb386b591b67e01e17f0e6c0f3
aheit <aheit@localhost.localdomain>
2016-12-18 19:55:43 +0100

    2291

--- tirage.c
+++ tirage.c
@@ -4,29 +4,79 @@
 #include <sys/wait.h>
 #include "user_agence.h"
 #include <time.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include "semaphore.h"
+#include "shmem.h"
+#include "mqueue.h"
 
-int rand_flight(int a, int b){
+int key_mutex = 100;
+int key_mutex2 = 150;
+int key_database = 200;
+int key_vols = 300;
+int key_presence = 350;
+int key_mqueue = 400;
+int size_shmem = 20;
 
-	srand(time(NULL));
-	int number = (rand()%(b-a))+a;
-	return number;
-}
-
-int ecrivain();
+int rand_flight(int a, int b);
+int tirage();
+void stopTirage();
+int ecrivain(int descripteur[2]);
+void stopEcrivain();
 
 int tirage(){
-
-//	signal(SIGINT,stopTirage);
-//	signal(SIGHUP,stopTirage);
-//	signal(SIGQUIT,stopTirage);
+	signal(SIGINT,stopTirage);
+	signal(SIGHUP,stopTirage);
+	signal(SIGQUIT,stopTirage);
 
 	Flight newflight;
 
 	int pid, descripteur[2], destination, nb_places;
-	int a = 1;
-	int b = 20;
-	int c = 100;
-	int d = 500;
+	int destMin = 1;
+	int destMax = 20;
+	int placeMin = 100;
+	int placeMax = 500;
+
+	//creation et initialisation sémaphore Mutex à 1
+	int semid_Mutex = create_semaphore(key_mutex);
+	if (semid_Mutex == -1)
+		exit(1);
+        init_semaphore(semid_Mutex, 1);
+
+	//création et initialisation sémaphore Mutex2 à 1
+	int semid_Mutex2 = create_semaphore(key_mutex2);
+        if (semid_Mutex2 == -1)
+                exit(1);
+        init_semaphore(semid_Mutex2, 1);
+
+	//Création de la BD
+	int shmem_id = create_shmem(key_database, size_shmem);
+	if (shmem_id == -1)
+		exit(1);
+
+	//création et initialisation sémaphore vols à 20
+	int semid_vols = create_semaphore(key_presence);
+        if (semid_vols == -1)
+                exit(1);
+        init_semaphore(semid_vols, 20);
+
+	//création et initialisation sémaphore présence à -3
+	int semid_presence = create_semaphore(key_presence);
+        if (semid_presence == -1)
+                exit(1);
+	init_semaphore(semid_presence, -3);
+
+	//création de la file de message
+	int mqueue_id = create_mqueue(key_mqueue);
+	if (mqueue_id == -1)
+		exit(1);
+
+	/*création du pipe avec prise en compte erreur création ou erreur fork
+	 cas du fils et du père
+	 si c'est le père on tire un vol, le met dans la struct, l'envoie a travers le pipe
+	 puis attend 10 secondes et recommence */
 
 	if(pipe(descripteur) < 0){
 		printf("erreur création pipe \n");
@@ -45,8 +95,8 @@ int tirage(){
 		while(1){
 
 			printf("Je suis Tirage et je tire un vol aléatoire\n");
-			destination = rand_flight(a,b);
-			nb_places = rand_flight(c,d);
+			destination = rand_flight(destMin,destMax);
+			nb_places = rand_flight(placeMin,placeMax);
 			newflight.destination = destination;
 			newflight.number = nb_places;
 			write(descripteur[1],&newflight,sizeof(Flight));
@@ -56,14 +106,34 @@ int tirage(){
 	}
 }
 
-
+void stopTirage(){
+	//envoie un sigint aux autres processus
+	//attend la mort de son fils
+	down(key_presence);
+	//Est ce qu'il faut fermer le pipe malgré qu'on ferme les accès dans les fcts
+	//Est ce qu'on détruit un pipe ??
+	remove_semaphore(key_mutex);
+	remove_semaphore(key_mutex2);
+	remove_shmem(key_database);
+	remove_semaphore(key_vols);
+	remove_mqueue(key_mqueue);
+	remove_semaphore(key_presence);
+	printf("Processus Tirage arrêté\n");
+	exit(0);
+}
 
 int ecrivain(int descripteur[2]){
 
-//	signal(SIGINT, stop);
-//      signal(SIGHUP,stop);
-//    signal(SIGQUIT,stop);
-Flight newflight;
+	signal(SIGINT, stopEcrivain);
+	signal(SIGHUP,stopEcrivain);
+	signal(SIGQUIT,stopEcrivain);
+
+	Flight newflight;
+
+	open_semaphore(key_mutex);
+	open_shmem(key_database,size_shmem);
+	open_semaphore(key_vols);
+	open_semaphore(key_presence);
 
 	close(descripteur[1]);
 	while(1){
@@ -72,10 +142,28 @@ Flight newflight;
 		printf("Le nombre de place disponible est %d\n",newflight.number);
 	}
 	close(descripteur[0]);
+}
 
+void stopEcrivain(){
+	remove_semaphore(key_mutex);
+	remove_shmem(key_database);
+	remove_semaphore(key_vols);
+	//même question pour le pipe en lecture
+	up(key_presence);
+	remove_semaphore(key_presence);
+	printf("Processus Ecrivain arrêté\n");
+	exit(0);
+}
+
+int rand_flight(int a, int b){
+
+        srand(time(NULL));
+        int number = (rand()%(b-a))+a;
+        return number;
 }
 
 int main(){
 tirage();
 return 0;
 }
+
